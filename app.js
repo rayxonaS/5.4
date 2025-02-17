23f3a1916aa69b5e1ccd2afd89789805aea2039d
rayxona <rayxona@gmail.com>
2025-02-17 12:28:03 +0500

    refactor app.js: clean up commented code and improve layout settings add .vscode/settings.json: configure live server port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //FIRST CARD
 
-/*const divWrapper = document.createElement("div");
+const divWrapper = document.createElement("div");
 divWrapper.style.borderRadius = "20px";
 divWrapper.style.display = "flex";
 divWrapper.style.backgroundColor = "white";
@@ -90,94 +90,4 @@ document.body.style.backgroundColor = "#F4D04E";
 document.body.style.display = "flex";
 document.body.style.justifyContent = "center";
 document.body.style.alignItems = "center";
-document.body.style.height = "100vh";*/
-
-//SECOND CARD
-
-/*const divWrapper = document.createElement("div");
-divWrapper.style.borderRadius = "12px";
-divWrapper.style.display = "flex";
-divWrapper.style.backgroundColor = "#1F1F1F";
-divWrapper.style.flexDirection = "column";
-divWrapper.style.maxWidth = "384px";
-divWrapper.style.alignItems = "center";
-divWrapper.style.padding = "40px";
-divWrapper.style.fontFamily = "Inter, sans-serif";
-divWrapper.style.color = "white";
-
-const avatarImage = document.createElement("img");
-avatarImage.src = "https://picsum.photos/id/30/88/88";
-avatarImage.style.borderRadius = "50%";
-avatarImage.style.marginTop = "40px";
-
-const title = document.createElement("h3");
-title.textContent = "Jessica Randall";
-title.style.fontFamily = "Inter, sans-serif";
-title.style.fontweight = "600";
-title.style.fontSize = "24px";
-title.style.lineHeight = "36px";
-title.style.color = "white";
-title.style.marginTop = "24px";
-
-const subtitle = document.createElement("h5");
-subtitle.textContent = "London, United Kingdom";
-subtitle.style.color = "#C4F82A";
-subtitle.style.fontSize = "14px";
-subtitle.style.lineHeight = "21px";
-subtitle.style.fontweight = "700";
-subtitle.style.marginTop = "4px";
-
-const text = document.createElement("p");
-text.textContent = "Front-end developer and avid reader.";
-text.style.fontSize = "14px";
-text.style.lineHeight = "21px";
-text.style.fontweight = "400";
-text.style.marginTop = "8px";
-text.style.textAlign = "center";
-
-const ul = document.createElement("ul");
-ul.style.listStyle = "none";
-ul.style.padding = "0";
-ul.style.marginTop = "24px";
-
-const linksText = [
-  "Github",
-  "Frontend Mentor",
-  "LinkedIn",
-  "Twitter",
-  "Instagram",
-];
-for (let i = 0; i < 5; i++) {
-  const li = document.createElement("li");
-  li.style.alignItems = "center";
-  li.style.display = "flex";
-  li.style.justifyContent = "center";
-  li.style.width = "304px";
-  li.style.height = "45px";
-  li.style.backgroundColor = "rgba(51, 51, 51, 1)";
-  li.style.padding = "12px";
-  li.style.borderRadius = "8px";
-  li.style.marginBottom = "8px";
-
-  const a = document.createElement("a");
-  a.style.textDecoration = "none";
-  a.style.textAlign = "center";
-  a.style.color = "white";
-  a.style.fontSize = "14px";
-  a.style.lineHeight = "21px";
-  a.style.fontweight = "400";
-  a.style.marginLeft = "auto";
-  a.style.marginRight = "auto";
-  a.href = "#";
-  a.textContent = linksText[i];
-  li.appendChild(a);
-  ul.appendChild(li);
-}
-
-divWrapper.append(avatarImage, title, subtitle, text, ul);
-document.body.appendChild(divWrapper);
-document.body.style.backgroundColor = "black";
-document.body.style.display = "flex";
-document.body.style.justifyContent = "center";
-document.body.style.alignItems = "center";
-document.body.style.height = "100vh";*/
+document.body.style.height = "100vh";
